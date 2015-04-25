4c1c8b327deb310a227797aee7987f2e34f44ec3
Anirudh <ani.for.prez@gmail.com>
2015-04-25 19:15:39 +0530

    autocomplete works

--- app/controllers/main.js
+++ app/controllers/main.js
@@ -1,16 +1,36 @@
 angular
 	.module('HousingApp')
-	.controller('MainCtrl', ['$scope', 'uiGmapGoogleMapApi', function($scope, uiGmapGoogleMapApi) {
+	.controller('MainCtrl', ['$scope', '$q', 'uiGmapGoogleMapApi', function($scope, $q, uiGmapGoogleMapApi) {
+		$scope.mapsLoaded = false;
 		$scope.cities = ['Delhi', 'Bangalore', 'Chennai', 'Kolkata'];
 		$scope.selectedCity = '';
 		$scope.map = { center: { latitude: 45, longitude: -73 }, zoom: 8, pan: false };
+
+		$scope.selectedItem = '';
+		$scope.searchText = '';
+
+		$scope.mapsAutocomplete = {};
+		$scope.getMatches = function(searchText) {
+			var q = $q.defer();
+			if($scope.mapsLoaded) {
+				$scope.mapsAutocomplete.getPlacePredictions({ input: searchText, componentRestrictions: { country: 'in' }, types: ['(regions)'] }, function(predictions, status) {
+					if (status != google.maps.places.PlacesServiceStatus.OK) {
+						alert(status);
+						q.resolve([]);
+					}
+					else {
+						q.resolve(predictions);
+					}
+				});
+			}
+			else {
+				q.resolve([]);
+			}
+			return q.promise;
+		};
+
 		uiGmapGoogleMapApi.then(function(maps) {
-			var service = new google.maps.places.AutocompleteService();
-			service.getQueryPredictions({ input: 'banga' }, function(predictions, status) {
-				if (status != google.maps.places.PlacesServiceStatus.OK) {
-					alert(status);
-					return;
-				}
-			});
+			$scope.mapsAutocomplete = new google.maps.places.AutocompleteService();
+			$scope.mapsLoaded = true;
 		});
 	}]);
